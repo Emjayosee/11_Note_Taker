6966c896b1c0ff1e694949a2b9c0e2ca7ca0f73e
Emjayosee <michael.j.oconnor@me.com>
2021-03-11 05:41:14 -0800

    Status following intial effort plus Tutorial with Eugene.

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const uuid = require('uuid');
 const app = express();
 
-const PORT = 3000; // process.env.PORT ?
+const PORT = process.env.PORT || 3000;
 
 // will share any static html files with the browser
 app.use( express.static('public') );
@@ -10,9 +10,9 @@ app.use( express.static('public') );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const dbFile = './app/db.json';
+const dbFile = '/db/db.json';
 
-let noteList = [{id: "0000-0000-0000-0000", title: 'note1', text: 'note1 text'}];
+let noteList = [{id: "0000-0000-0000-0000", title: 'To Do', text: 'Finish Assignment 11'}];
 
 // Endpoints =================================================
 
@@ -20,6 +20,30 @@ let noteList = [{id: "0000-0000-0000-0000", title: 'note1', text: 'note1 text'}]
 // note: for app.post: newNote.id = uuid.v4() // use a random unique id.
 // ... code away ...
 
+app.get('/notes', function(req, res) {
+    // get the note info
+    res.sendFile('public/notes.html' , { root : __dirname});
+});
+
+app.get('/api/notes', function(req, res) {
+    // get the note info
+    res.send(JSON.stringify(noteList));
+});
+
+app.post('/api/notes', function(req, res) {
+    // get the note info
+    console.log(req.body)
+    var newNote = [];
+    newNote.id = uuid.v4()
+    noteList.push(newNote);
+    console.log(newNote.id)
+    res.send(JSON.stringify(noteList));
+});
+
+// app.delete('/api/notes ; deleteNote', function(req, res) {
+//
+// res.send( noteData );
+// });
 
 // Listener ==================================================
 app.listen(PORT, function() {
